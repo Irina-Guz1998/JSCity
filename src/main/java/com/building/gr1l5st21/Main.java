aa3c470e8ba4ebfc1eca3c043ff09b1f34fc520e
roma <roma@example.com>
2018-01-21 01:20:55 +0200

    added buildings from other groups

---
+++ src/main/java/com/building/gr1l5st21/Main.java
@@ -0,0 +1,21 @@
+package com.building.gr1l5st21;
+
+import eu.printingin3d.javascad.exceptions.IllegalValueException;
+import eu.printingin3d.javascad.utils.SaveScadFiles;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    /**
+     * @param args
+     * @throws IOException
+     * @throws IllegalValueException
+     */
+    public static void main(String[] args) throws IllegalValueException, IOException {
+        new SaveScadFiles(new File("./target")).
+                addModel("building_gr1l5st21.scad", new Building(2,1)).
+                saveScadFiles();
+    }
+}
